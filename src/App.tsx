5340d3a50b45235d7dc2ae87d5a2b712f61fedd7
Mirhasankhan <mirhasan000034@gmail.com>
2024-11-07 14:08:36 +0600

    task completed

--- src/App.tsx
+++ src/App.tsx
@@ -17,9 +17,13 @@ function App() {
   const toDo = tasks?.filter(
     (task: { status: string }) => task.status == "To Do"
   );
-  // const doing = tasks?.filter((task) => task.status == "Doing");
-  // const underReview = tasks?.filter((task) => task.status == "Under Review");
-  // const completed = tasks?.filter((task) => task.status == "Completed");
+  const doing = tasks?.filter(
+    (task: { status: string }) => task.status == "Doing"
+  );
+  const completed = tasks?.filter(
+    (task: { status: string }) => task.status == "Completed"
+  );
+  console.log(completed);
 
   const style: CustomCSSProperties = {
     scrollbarWidth: "thin",
@@ -32,11 +36,7 @@ function App() {
 
   return (
     <div className="grid grid-cols-4 gap-3 m-8">
-      <div
-        style={style}
-        className="bg-gray-200 p-1 h-screen overflow-auto"
-        key={status}
-      >
+      <div style={style} className="bg-gray-200 p-1 h-screen overflow-auto">
         <div className="flex justify-between items-center">
           <div className="flex items-center gap-1 ">
             <h1 className="h-8 w-8 bg-red-500 rounded-l-full"></h1>
@@ -44,7 +44,49 @@ function App() {
           </div>
           <h1 className="bg-gray-300 px-3 rounded-md">{incomplete?.length}</h1>
         </div>
+        {incomplete?.map((ins: { _id: string }) => (
+          <div key={ins._id}>
+            <Card task={ins}></Card>
+          </div>
+        ))}
+      </div>
+      <div style={style} className="bg-gray-200 p-1 h-screen overflow-auto">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-1 ">
+            <h1 className="h-8 w-8 bg-red-500 rounded-l-full"></h1>
+            <h1 className="font-semibold">To Do</h1>
+          </div>
+          <h1 className="bg-gray-300 px-3 rounded-md">{toDo?.length}</h1>
+        </div>
         {toDo?.map((ins: { _id: string }) => (
+          <div key={ins._id}>
+            <Card task={ins}></Card>
+          </div>
+        ))}
+      </div>
+      <div style={style} className="bg-gray-200 p-1 h-screen overflow-auto">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-1 ">
+            <h1 className="h-8 w-8 bg-red-500 rounded-l-full"></h1>
+            <h1 className="font-semibold">Doing</h1>
+          </div>
+          <h1 className="bg-gray-300 px-3 rounded-md">{doing?.length}</h1>
+        </div>
+        {doing?.map((ins: { _id: string }) => (
+          <div key={ins._id}>
+            <Card task={ins}></Card>
+          </div>
+        ))}
+      </div>
+      <div style={style} className="bg-gray-200 p-1 h-screen overflow-auto">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-1 ">
+            <h1 className="h-8 w-8 bg-red-500 rounded-l-full"></h1>
+            <h1 className="font-semibold">Completed</h1>
+          </div>
+          <h1 className="bg-gray-300 px-3 rounded-md">{completed?.length}</h1>
+        </div>
+        {completed?.map((ins: { _id: string }) => (
           <div key={ins._id}>
             <Card task={ins}></Card>
           </div>
